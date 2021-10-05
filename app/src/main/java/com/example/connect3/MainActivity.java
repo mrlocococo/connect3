2aba6709bdecce576e174aa0b43d6cc7680b6c64
Marco <gsoccerstar007@gmail.com>
2021-10-05 17:27:51 -0400

    added onClick to all buttons

---
+++ app/src/main/java/com/example/connect3/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.connect3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1500);
+
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
