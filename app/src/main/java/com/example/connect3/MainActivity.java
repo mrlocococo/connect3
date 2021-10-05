31dda787436dc188ae642603f68e2e873fd79abd
Marco Polo <87492267+mrlocococo@users.noreply.github.com>
2021-10-05 18:37:48 -0400

    Update MainActivity.java
    Added ability to replay game, replaced toast with textview and pops up when someone has won, added a button to play again.

--- app/src/main/java/com/example/connect3/MainActivity.java
+++ app/src/main/java/com/example/connect3/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.connect3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,45 +21,101 @@ public class MainActivity extends AppCompatActivity {
 
     int activePlayer = 0;
 
+    boolean gameActive = true;
+
     public void dropIn(View view) {
 
         ImageView counter = (ImageView) view;
 
-        Log.i("Tag", counter.getTag().toString());
-
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        gameState[tappedCounter] = activePlayer;
+        if (gameState[tappedCounter] == 2 && gameActive) {
 
-        counter.setTranslationY(-1500);
+            gameState[tappedCounter] = activePlayer;
 
-        if (activePlayer == 0) {
+            counter.setTranslationY(-1500);
 
-            counter.setImageResource(R.drawable.yellow);
+            if (activePlayer == 0) {
 
-            activePlayer = 1;
+                counter.setImageResource(R.drawable.yellow);
 
-        } else {
+                activePlayer = 1;
 
-            counter.setImageResource(R.drawable.red);
+            } else {
 
-            activePlayer = 0;
+                counter.setImageResource(R.drawable.red);
 
-        }
-
-        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
-
-        for (int[] winningPosition : winningPositions) {
-
-            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] !=2) {
-
-                // Someone has won!
-
-                Toast.makeText(this, "Someone has won!", Toast.LENGTH_SHORT).show();
+                activePlayer = 0;
 
             }
 
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+            for (int[] winningPosition : winningPositions) {
+
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+
+                    // Someone has won!
+
+                    gameActive = false;
+
+                    String winner = "";
+
+                    if (activePlayer == 1) {
+
+                        winner = "Yellow";
+
+                    } else {
+
+                        winner = "Red";
+
+                    }
+
+                    Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+
+                    TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+
+                    winnerTextView.setText(winner + " has won!");
+
+                    playAgainButton.setVisibility(View.VISIBLE);
+
+                    winnerTextView.setVisibility(View.VISIBLE);
+
+                }
+
+            }
         }
+    }
+
+    public void playAgain(View view) {
+
+        Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+
+        TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+
+        playAgainButton.setVisibility(View.INVISIBLE);
+
+        winnerTextView.setVisibility(View.INVISIBLE);
+
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+
+        for(int i=0; i<gridLayout.getChildCount(); i++) {
+
+           ImageView counter = (ImageView) gridLayout.getChildAt(i);
+
+           counter.setImageDrawable(null);
+
+        }
+
+        for (int i=0; i<gameState.length; i++){
+
+            gameState[i] = 2;
+
+        }
+
+        activePlayer = 0;
+
+        gameActive = true;
 
     }
 
